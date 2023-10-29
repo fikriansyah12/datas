1f4c8fbb506169ac4e9fff3a3c1f3d02651d1606
fikriansyah12 <fikriansyahmuhammad664.com>
2023-10-29 21:01:43 +0700

    CORRECTION

---
+++ code_1.py
@@ -0,0 +1,15 @@
+rows = int(input("DATA = "))
+print("===SCIENCE====")
+
+for i in range(1, rows + 1):
+    for j in range(1, rows - i + 1):
+        print(end=' ')
+        for k in range(1, (2*i)):
+            print(k, end='')
+            print()
+for i in range(rows, -1, 0, -1):
+    for j in range(1, (rows-i+1)):
+        print(end=' ')
+        for k in range(1, (2 * i)):
+            print(k, end=' ')
+            print()
